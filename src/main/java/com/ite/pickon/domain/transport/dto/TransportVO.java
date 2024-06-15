37e6025d713459db2756475c6ec18395fa6d1f5b
Hypersand <on8214@gmail.com>
2024-06-16 01:16:53 +0900

    feat: 출발 배차 시간되면 배송중으로 상태 변경한는 배치 메서드 구현

--- src/main/java/com/ite/pickon/domain/transport/dto/TransportVO.java
+++ src/main/java/com/ite/pickon/domain/transport/dto/TransportVO.java
@@ -1,5 +1,15 @@
 package com.ite.pickon.domain.transport.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Getter
+@AllArgsConstructor
 public class TransportVO {
+    private int fromStoreId;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
 
 }
