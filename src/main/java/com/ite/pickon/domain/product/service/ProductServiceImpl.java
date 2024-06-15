cf38d5f0f94d5a4ed9734a5631d8f13cad73a19b
Hypersand <on8214@gmail.com>
2024-06-15 17:46:29 +0900

    feat: ProductService 구현

--- src/main/java/com/ite/pickon/domain/product/service/ProductServiceImpl.java
+++ src/main/java/com/ite/pickon/domain/product/service/ProductServiceImpl.java
@@ -1,5 +1,20 @@
 package com.ite.pickon.domain.product.service;
 
-public class ProductServiceImpl implements ProductService {
+import com.ite.pickon.domain.product.dto.ProductAdminVO;
+import com.ite.pickon.domain.product.mapper.ProductMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductMapper productMapper;
+
+    @Override
+    public List<ProductAdminVO> findProductList(String storeId, Pageable pageable, String keyword) {
+        return productMapper.selectProductListByStore(storeId, pageable, keyword);
+    }
 }
