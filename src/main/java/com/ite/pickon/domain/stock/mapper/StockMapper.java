5caef020ef471ac1b943c44de77ee719c970a370
Hypersand <on8214@gmail.com>
2024-06-15 22:39:35 +0900

    feat: 지점별 특정 상품 재고 조회 mapper 작성

--- src/main/java/com/ite/pickon/domain/stock/mapper/StockMapper.java
+++ src/main/java/com/ite/pickon/domain/stock/mapper/StockMapper.java
@@ -1,5 +1,13 @@
 package com.ite.pickon.domain.stock.mapper;
 
+import com.ite.pickon.domain.stock.dto.StockVO;
+
+import java.util.List;
+
 public interface StockMapper {
+
+    // 지점별 상품의 재고 조회
+    List<StockVO> selectStockForStore(int productId);
+
 
 }
