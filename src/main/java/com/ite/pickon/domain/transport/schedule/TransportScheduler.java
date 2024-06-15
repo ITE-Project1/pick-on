6504847f9bc15efd3369b656b7b6816883ee293a
Hypersand <on8214@gmail.com>
2024-06-16 01:16:53 +0900

    feat: 출발 배차 시간되면 배송중으로 상태 변경한는 배치 메서드 구현

--- src/main/java/com/ite/pickon/domain/transport/schedule/TransportScheduler.java
+++ src/main/java/com/ite/pickon/domain/transport/schedule/TransportScheduler.java
@@ -1,19 +1,36 @@
 package com.ite.pickon.domain.transport.schedule;
 
+import com.ite.pickon.domain.transport.dto.TransportVO;
+import com.ite.pickon.domain.transport.service.TransportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 @Component
-@EnableScheduling
 @RequiredArgsConstructor
+@EnableScheduling
 public class TransportScheduler {
     private final TaskScheduler taskScheduler;
+    private final TransportService transportService;
+    public void changeTransportStatus(String productId,
+                                      int quantity,
+                                      int storeId,
+                                      Date orderDate,
+                                      String orderId) {
+        TransportVO transportVO = transportService.findOptimalTransportStore(productId, quantity, storeId, orderDate);
 
-    @Scheduled(fixedDelay=10000)
-    public void init() {
-        System.out.println("동작여부 확인");
+        LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), transportVO.getDepartureTime());
+        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        Date date2 = Date.from(LocalDateTime.now().plusMinutes(1).atZone(ZoneId.systemDefault()).toInstant());
+        //지점 연결 후 출발 배차 시간이 되면 배송중으로 상태 변경(배치) PENDING → SHIPPED
+        taskScheduler.schedule(() -> transportService.modifyTransportStatus(orderId), date2);
     }
 }
