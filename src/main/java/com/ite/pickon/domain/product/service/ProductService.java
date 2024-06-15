bd5e88f6448d29b66d016c5fdff3d66e2b7ebacb
Hypersand <on8214@gmail.com>
2024-06-15 17:46:29 +0900

    feat: ProductService 구현

--- src/main/java/com/ite/pickon/domain/product/service/ProductService.java
+++ src/main/java/com/ite/pickon/domain/product/service/ProductService.java
@@ -1,5 +1,10 @@
 package com.ite.pickon.domain.product.service;
 
-public interface ProductService {
+import com.ite.pickon.domain.product.dto.ProductAdminVO;
+import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
+public interface ProductService {
+    List<ProductAdminVO> findProductList(String storeId, Pageable pageable, String keyword);
 }
