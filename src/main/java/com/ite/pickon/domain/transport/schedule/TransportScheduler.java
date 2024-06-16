3968013632d962d47ecc7e627e3bbf90e2179e03
Hypersand <on8214@gmail.com>
2024-06-16 15:26:27 +0900

    feat: @Scheduled로 배차 상태 변경 로직 수정

--- src/main/java/com/ite/pickon/domain/transport/schedule/TransportScheduler.java
+++ src/main/java/com/ite/pickon/domain/transport/schedule/TransportScheduler.java
@@ -1,34 +1,41 @@
 package com.ite.pickon.domain.transport.schedule;
 
-import com.ite.pickon.domain.transport.dto.TransportVO;
+import com.ite.pickon.domain.transport.TransportSchedule;
 import com.ite.pickon.domain.transport.service.TransportService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 @EnableScheduling
 public class TransportScheduler {
-    private final TaskScheduler taskScheduler;
     private final TransportService transportService;
-    public void changeTransportStatus(String productId,
-                                      int quantity,
-                                      int storeId,
-                                      Date orderDate,
-                                      String orderId) {
-        TransportVO transportVO = transportService.findOptimalTransportStore(productId, quantity, storeId, orderDate);
 
-        LocalDateTime localDateTime = LocalDateTime.of(LocalDate.now(), transportVO.getDepartureTime());
-        Date date = Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-
-        //지점 연결 후 출발 배차 시간이 되면 배송중으로 상태 변경(배치) PENDING → SHIPPED
-        taskScheduler.schedule(() -> transportService.modifyTransportStatus(orderId), date);
+    @Scheduled(cron = "0 0 8 * * *") // 매일 08:00
+    public void scheduleTaskForStore1() {
+        transportService.modifyTransportStatus(TransportSchedule.STORE_1.getStoreId());
     }
+
+    @Scheduled(cron = "0 0 9 * * *") // 매일 09:00
+    public void scheduleTaskForStore2() {
+        transportService.modifyTransportStatus(TransportSchedule.STORE_2.getStoreId());
+    }
+
+    @Scheduled(cron = "0 0 10 * * *") // 매일 10:00
+    public void scheduleTaskForStore3() {
+        transportService.modifyTransportStatus(TransportSchedule.STORE_3.getStoreId());
+    }
+
+    @Scheduled(cron = "0 0 11 * * *") // 매일 11:00
+    public void scheduleTaskForStore4() {
+        transportService.modifyTransportStatus(TransportSchedule.STORE_4.getStoreId());
+    }
+
+    @Scheduled(cron = "0 0 12 * * *") // 매일 12:00
+    public void scheduleTaskForStore5() {
+        transportService.modifyTransportStatus(TransportSchedule.STORE_5.getStoreId());
+    }
+
 }
--- src/main/java/com/ite/pickon/domain/transport/service/TransportService.java
+++ src/main/java/com/ite/pickon/domain/transport/service/TransportService.java
@@ -10,5 +10,5 @@ public interface TransportService {
                                           int storeId,
                                           Date orderDate);
 
-    void modifyTransportStatus(String orderId);
+    void modifyTransportStatus(int fromStoreId);
 }
