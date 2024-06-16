2ca2019b03356944e49b7164faf263a7342866e8
Chris <florida999@daum.net>
2024-06-16 20:05:04 +0900

    feat: createProduct, getProductDetail

--- src/main/java/com/ite/pickon/domain/product/controller/ProductController.java
+++ src/main/java/com/ite/pickon/domain/product/controller/ProductController.java
@@ -1,6 +1,9 @@
 package com.ite.pickon.domain.product.controller;
 
 import com.ite.pickon.domain.product.dto.ProductAdminVO;
+import com.ite.pickon.domain.product.dto.ProductRequest;
+import com.ite.pickon.domain.product.dto.ProductResponse;
+import com.ite.pickon.domain.product.dto.ProductVO;
 import com.ite.pickon.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -35,6 +38,27 @@ public class ProductController {
 
         Pageable pageable = PageRequest.of(page, 10, sortOrder);
         return new ResponseEntity<>(productService.findProductList(storeId, pageable, keyword), HttpStatus.OK);
+    }
+//상품등록
+    @PostMapping("/register")
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        if (productService.insertProduct(productRequest)==1){
+            System.out.println("insert success"); // 이렇게 처리해도 되나?
+        };
+        //service단에서 throws 하기
+    }
+//상품 상세조회
+    @GetMapping("/detail/{productId}")
+    public ResponseEntity<ProductResponse> getProductDetail(@PathVariable String productId){
+        System.out.println("====================");
+        //return new ResponseEntity<>(productService.getDetail(productId),HttpStatus.OK);
+        //ResponseEntity는 HTTP 응답을 나타내는 클래스로서, 클라이언트에게 반환할 데이터와 HTTP 상태 코드를 포함
+        ProductResponse response = productService.getDetail(productId);
+        // ProductResponse 객체 로깅
+        System.out.println(response);
+
+        // ResponseEntity 생성 후 반환
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
--- src/main/java/com/ite/pickon/domain/product/service/ProductService.java
+++ src/main/java/com/ite/pickon/domain/product/service/ProductService.java
@@ -1,10 +1,16 @@
 package com.ite.pickon.domain.product.service;
 
-import com.ite.pickon.domain.product.dto.ProductAdminVO;
+import com.ite.pickon.domain.product.dto.*;
+
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ProductService {
     List<ProductAdminVO> findProductList(String storeId, Pageable pageable, String keyword);
+    int insertProduct(ProductRequest productRequest);
+
+    ProductResponse getDetail(String productId);
 }
+
+
