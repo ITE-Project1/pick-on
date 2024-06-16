a647e7a2b32035ff0165b02e2d5381892e4e102e
serak0310 <serak0310@naver.com>
2024-06-16 14:37:11 +0900

    feat: 지점별 주문 목록 조회
    관리자 페이지 - 지점별 주문 목록 조회 및 키워드 검색 api

--- src/main/java/com/ite/pickon/domain/order/controller/OrderController.java
+++ src/main/java/com/ite/pickon/domain/order/controller/OrderController.java
@@ -1,19 +1,21 @@
 package com.ite.pickon.domain.order.controller;
 
-import com.ite.pickon.domain.order.dto.GetOrderRes;
-import com.ite.pickon.domain.order.dto.PostOrderReq;
+import com.ite.pickon.domain.order.OrderStatus;
+import com.ite.pickon.domain.order.dto.MultiOrderRes;
+import com.ite.pickon.domain.order.dto.OrderRes;
 import com.ite.pickon.domain.order.service.OrderService;
+import com.ite.pickon.response.SimpleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -21,6 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     private final OrderService orderService;
+
+    private static final int PAGE_SIZE = 10;
 
 //    /**
 //     * 주문하기
@@ -35,12 +39,39 @@ public class OrderController {
 //    }
 
     /**
+     * 지점별 주문 내역 조회
+     */
+    @GetMapping("")
+    public ResponseEntity<List<MultiOrderRes>> getOrdersByStoreId(@RequestParam String storeId,
+                                                                  @RequestParam int page,
+                                                                  @RequestParam(required = false) String keyword) {
+
+        return new ResponseEntity<>(orderService.getOrdersByStoreId(storeId, page, PAGE_SIZE, keyword), HttpStatus.OK);
+    }
+
+
+    /**
      *  주문 상세조회
      */
     @GetMapping("/{orderId}")
-    public ResponseEntity<GetOrderRes> getOrderById(@PathVariable String orderId) {
-        GetOrderRes getOrderRes = orderService.getOrderById(orderId);
-        return new ResponseEntity<>(getOrderRes, HttpStatus.OK);
+    public ResponseEntity<OrderRes> getOrderById(@PathVariable String orderId) {
+        OrderRes orderRes = orderService.getOrderById(orderId);
+        return new ResponseEntity<>(orderRes, HttpStatus.OK);
     }
+
+    /**
+     *  고객 픽업 완료
+     */
+    @PatchMapping(value = "/{orderId}/status/completed", produces = "application/json; charset=UTF-8")
+    public ResponseEntity<SimpleResponse> updateOrderAsCompleted(@PathVariable String orderId) {
+        orderService.updateOrderStatus(orderId, OrderStatus.COMPLETED);
+        return new ResponseEntity<>(new SimpleResponse("고객 픽업이 완료되었습니다."), HttpStatus.OK);
+    }
+
+//    @PatchMapping("/{orderId}/status/pickupready")
+//    public ResponseEntity<String> updateOrderStatusToPickupReady(@PathVariable String orderId) {
+//        orderService.updateOrderStatus(orderId, OrderStatus.PICKUPREADY);
+//        return new ResponseEntity<>("주문 상태가 PICKUPREADY로 업데이트되었습니다.", HttpStatus.OK);
+//    }
 
 }
---
+++ src/main/java/com/ite/pickon/response/SimpleResponse.java
@@ -0,0 +1,10 @@
+package com.ite.pickon.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class SimpleResponse {
+    private String message;
+}
