46ea642b47290d2f8e834d90780c667763eabee8
yjpark <ox8764@naver.com>
2024-06-16 20:23:51 +0900

    fix: 병합 충돌 수정

--- src/main/java/com/ite/pickon/domain/order/dto/OrderVO.java
+++ src/main/java/com/ite/pickon/domain/order/dto/OrderVO.java
@@ -1,5 +1,21 @@
 package com.ite.pickon.domain.order.dto;
 
-public class OrderVO {
+import lombok.Builder;
+import lombok.Getter;
 
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class OrderVO {
+    private String orderId;
+    private Long userId;
+    private Long storeId;
+    private String productId;
+    private LocalDateTime pickupDate;
+    private int quantity;
+    private double totalPrice;
+    private int status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/ite/pickon/domain/order/service/OrderServiceImpl.java
+++ src/main/java/com/ite/pickon/domain/order/service/OrderServiceImpl.java
@@ -1,5 +1,33 @@
 package com.ite.pickon.domain.order.service;
 
+import com.ite.pickon.domain.order.dto.GetOrderRes;
+import com.ite.pickon.domain.order.dto.PostOrderReq;
+import com.ite.pickon.domain.order.mapper.OrderMapper;
+import com.ite.pickon.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.ite.pickon.exception.ErrorCode.FIND_FAIL_ORDER_ID;
+
+@Service
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderMapper orderMapper;
+
+    // 주문하기
+    @Override
+    public void createOrder(Long userId, PostOrderReq orderReq) {
+
+    }
+
+    // 주문 상세조회
+    @Override
+    public GetOrderRes getOrderById(String orderId) {
+        GetOrderRes order = orderMapper.selectOrderById(orderId);
+        if (order == null) {
+            throw new CustomException(FIND_FAIL_ORDER_ID);
+        }
+        return order;
+    }
 }
