c35ecdb31df5182a4e739655d6e04cdece0a3377
serak0310 <serak0310@naver.com>
2024-06-17 15:38:49 +0900

    feat: 재고 등록
    - 해당 지점에 기존에 상품에 대한 재고가 존재할 시, 재고 등록할 수 없음

--- src/main/java/com/ite/pickon/domain/stock/service/StockServiceImpl.java
+++ src/main/java/com/ite/pickon/domain/stock/service/StockServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ite.pickon.domain.stock.service;
 
+import com.ite.pickon.domain.stock.dto.StockReq;
 import com.ite.pickon.domain.stock.mapper.StockMapper;
+import com.ite.pickon.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.ite.pickon.exception.ErrorCode.EXISTS_STOCK_AT_STORE;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +15,21 @@ public class StockServiceImpl implements StockService {
 
     private final StockMapper stockMapper;
 
+    @Override
     @Transactional
     public void updateStock(int storeId, String productId, int quantityChange) {
         stockMapper.updateStockQuantity(storeId, productId, quantityChange);
+    }
+
+    @Override
+    @Transactional
+    public void addStock(StockReq stockReq) {
+        // 해당 지점에 기존에 재고가 등록되었는지 확인
+        int count = stockMapper.checkStockExists(stockReq.getStoreId(), stockReq.getProductId());
+        if (count>0) {
+            throw new CustomException(EXISTS_STOCK_AT_STORE);
+        }
+        // 재고 생성
+        stockMapper.insertStock(stockReq);
     }
 }
--- src/main/java/com/ite/pickon/exception/ErrorCode.java
+++ src/main/java/com/ite/pickon/exception/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
     FIND_FAIL_USER_ID(400, "존재하지 않는 유저입니다."),
     FIND_FAIL_PRODUCT_ID(400, "존재하지 않는 상품입니다."),
     FAIL_ORDER_BY_QUANTITY(400, "재고가 충분하지 않아 주문이 불가능합니다."),
+    EXISTS_STOCK_AT_STORE(400, "해당 지점에 이미 상품이 존재합니다."),
     FIND_FAIL_ORDER_ID(400, "존재하지 않는 주문입니다.");
 
     private final int status;
